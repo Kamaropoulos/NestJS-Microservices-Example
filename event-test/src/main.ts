9647a839a45fde205214db771b2cf61792c988ff
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-03 19:17:54 +0200

    feat: Init microservice for event-test project

--- event-test/src/main.ts
+++ event-test/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: { retryAttempts: 5, retryDelay: 3000, host: '0.0.0.0',port: 3010 },
+  });
+
+  await app.startAllMicroservicesAsync();
   await app.listen(3000);
 }
 bootstrap();
