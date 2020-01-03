78fbfce0bb72e2d03f96e2db1857fbc8ab1a9eb8
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-03 17:45:49 +0200

    feat: Update messaging service configuration

--- messaging-test/src/main.ts
+++ messaging-test/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.TCP,
-    options: { retryAttempts: 5, retryDelay: 3000 },
+    options: { retryAttempts: 5, retryDelay: 3000, host: '0.0.0.0',port: 3001},
   });
   await app.listenAsync();
 }
