1eafbd2b9122a1ecbd2c31cc148fb6d7de42968a
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-03 19:19:14 +0200

    feat: import event service on messaging-test's app module

--- messaging-test/src/app.module.ts
+++ messaging-test/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EVENT_SERVICE, EVENT_SERVICE_HOST, EVENT_SERVICE_PORT } from './app.constants';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([{ name: EVENT_SERVICE, transport: Transport.TCP, options: {host: EVENT_SERVICE_HOST, port: EVENT_SERVICE_PORT}}]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
