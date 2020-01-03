af830f389762d1c7f0bbd0564bd50b5673858d88
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-03 19:19:40 +0200

    feat: Add messaging-test REST API endpoint

---
+++ messaging-test/src/app.constants.ts
@@ -0,0 +1,3 @@
+export const EVENT_SERVICE = 'EVENT_SERVICE';
+export const EVENT_SERVICE_HOST = 'localhost';
+export const EVENT_SERVICE_PORT = 3010;
--- messaging-test/src/app.controller.ts
+++ messaging-test/src/app.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { MessagePattern, ClientProxy } from '@nestjs/microservices';
+import { EVENT_SERVICE } from './app.constants';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject(EVENT_SERVICE) private readonly client: ClientProxy) {}
 
   @MessagePattern({ cmd: 'echo' })
   msgEcho(data: String): String {
     return "You sent: " + data;
+  }
+
+  @Get()
+  getEchoService(): string {
+    this.client.emit("endpoint_hit", "hello event service!");
+    return "Success!";
   }
   
 }
