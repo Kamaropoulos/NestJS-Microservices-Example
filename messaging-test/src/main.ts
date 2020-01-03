1300fc3c5e9802bd67750c9359436362d544d43b
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-03 17:17:02 +0200

    feat: Update messaging-test main.ts to run a microservice

--- messaging-test/src/main.ts
+++ messaging-test/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: { retryAttempts: 5, retryDelay: 3000 },
+  });
+  await app.listenAsync();
 }
 bootstrap();
