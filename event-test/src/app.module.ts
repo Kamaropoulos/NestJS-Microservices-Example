68829e9c79ba2570dc2898a43db544bdcf37469b
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-03 17:48:43 +0200

    feat: Setup events service app module
    Import messaging service and remove app.service which is not needed.

--- event-test/src/app.module.ts
+++ event-test/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ECHO_SERVICE, ECHO_SERVICE_HOST, ECHO_SERVICE_PORT } from './app.constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([{ name: ECHO_SERVICE, transport: Transport.TCP, options: {host: ECHO_SERVICE_HOST, port: ECHO_SERVICE_PORT}}]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
