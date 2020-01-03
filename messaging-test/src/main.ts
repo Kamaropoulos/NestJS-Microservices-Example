37fd15a02a42b095b9ff9a18ac595dcbfa542074
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-03 19:19:57 +0200

    chore: Update ports

--- messaging-test/src/main.ts
+++ messaging-test/src/main.ts
@@ -3,10 +3,13 @@ import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
     transport: Transport.TCP,
-    options: { retryAttempts: 5, retryDelay: 3000, host: '0.0.0.0',port: 3001},
+    options: { retryAttempts: 5, retryDelay: 3000, host: '0.0.0.0',port: 3011 },
   });
-  await app.listenAsync();
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(3001);
 }
 bootstrap();
